bb2b8df2ee612faf4777c2a20d6c41c6d701a99b
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-01-08 01:11:12 +0600

    second

---
+++ Trait/bank.php
@@ -0,0 +1,12 @@
+<?php 
+
+trait Bank {
+    public function abc(){
+        echo "Its comes from abc function";
+    }
+
+    public function efg(){
+        echo "Its comes from efg function";
+    }
+}
+?>
